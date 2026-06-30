1374579c977cc31a5b50216e984f5f99a0aceda5
BRIX <brix@example.com>
2026-06-30 22:23:35 +0300

    updated

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import axios from "../apiConfig";
 import { clearAdminAuth, storeAdminAuth } from "../adminAccess";
 
 function AdminLogin() {
